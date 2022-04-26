fbd18eddf5607d514b41b428c9494a439481d367
Jack Thompson <jack.thompson1994@gmail.com>
2022-04-26 15:31:05 +0200

    Added initial "Piece-Square" tables and evaluate function

--- ChessV2/Bishop.cs
+++ ChessV2/Bishop.cs
@@ -4,8 +4,76 @@ namespace ChessV2
 {
     public class Bishop : Piece
     {
+        private readonly int[] Ranks = { 8, 7, 6, 5, 4, 3, 2, 1 };
         private const string BlackBishop = "\u265D";
         private const string WhiteBishop = "\u2657";
+        private readonly Dictionary<(int, int), int> Squares = new Dictionary<(int, int), int>()
+        {
+            [(1, 1)] = -20,
+            [(1, 2)] = -10,
+            [(1, 3)] = -10,
+            [(1, 4)] = -10,
+            [(1, 5)] = -10,
+            [(1, 6)] = -10,
+            [(1, 7)] = -10,
+            [(1, 8)] = -20,
+            [(2, 1)] = -10,
+            [(2, 2)] = 50,
+            [(2, 3)] = 0,
+            [(2, 4)] = 0,
+            [(2, 5)] = 0,
+            [(2, 6)] = 0,
+            [(2, 7)] = 5,
+            [(2, 8)] = -10,
+            [(3, 1)] = -10,
+            [(3, 2)] = 10,
+            [(3, 3)] = 10,
+            [(3, 4)] = 10,
+            [(3, 5)] = 10,
+            [(3, 6)] = 10,
+            [(3, 7)] = 10,
+            [(3, 8)] = -10,
+            [(4, 1)] = -10,
+            [(4, 2)] = 0,
+            [(4, 3)] = 10,
+            [(4, 4)] = 10,
+            [(4, 5)] = 10,
+            [(4, 6)] = 10,
+            [(4, 7)] = 0,
+            [(4, 8)] = -10,
+            [(5, 1)] = -10,
+            [(5, 2)] = 5,
+            [(5, 3)] = 5,
+            [(5, 4)] = 10,
+            [(5, 5)] = 10,
+            [(5, 6)] = 5,
+            [(5, 7)] = 5,
+            [(5, 8)] = -10,
+            [(6, 1)] = -10,
+            [(6, 2)] = 0,
+            [(6, 3)] = 5,
+            [(6, 4)] = 10,
+            [(6, 5)] = 10,
+            [(6, 6)] = 5,
+            [(6, 7)] = 0,
+            [(6, 8)] = -10,
+            [(7, 1)] = -10,
+            [(7, 2)] = 0,
+            [(7, 3)] = 0,
+            [(7, 4)] = 0,
+            [(7, 5)] = 0,
+            [(7, 6)] = 0,
+            [(7, 7)] = 0,
+            [(7, 8)] = -10,
+            [(8, 1)] = -20,
+            [(8, 2)] = -10,
+            [(8, 3)] = -10,
+            [(8, 4)] = -10,
+            [(8, 5)] = -10,
+            [(8, 6)] = -10,
+            [(8, 7)] = -10,
+            [(8, 8)] = -10,
+        };
 
         public List<(int, int)> BaseMoves = new List<(int, int)> { (1, 1), (1, -1), (-1, -1), (-1, 1), };
 
@@ -27,6 +95,15 @@ namespace ChessV2
         public override List<(int, int)> GetNextMoves((int, int) move)
         {
             return QueenBishopRook(move);
+        }
+
+        public override double Evaluate()
+        {
+            if (!Colour)
+            {
+                return PointsValue + Squares[(AIposition.Item1, Ranks[AIposition.Item2 - 1])];
+            }
+            return PointsValue + Squares[AIposition];
         }
     }
 }
--- ChessV2/Pawn.cs
+++ ChessV2/Pawn.cs
@@ -4,14 +4,80 @@ namespace ChessV2
 {
     public class Pawn : Piece
     {
-
+        private readonly int[] Ranks = { 8, 7, 6, 5, 4, 3, 2, 1 };
         private const string BlackPawn = "\u265f";
         private const string WhitePawn = "\u2659";
         public List<(int, int)> FirstMovesWhite = new List<(int, int)> { (0, 1), (0, 2) };
         public List<(int, int)> BaseMovesWhite = new List<(int, int)> { (0, 1) };
         public List<(int, int)> FirstMovesBlack = new List<(int, int)> { (0, -1), (0, -2) };
         public List<(int, int)> BaseMovesBlack = new List<(int, int)> { (0, -1) };
-
+        private readonly Dictionary<(int, int), int> Squares = new Dictionary<(int, int), int>()
+        {
+            [(1, 1)] = 0,
+            [(1, 2)] = 0,
+            [(1, 3)] = 0,
+            [(1, 4)] = 0,
+            [(1, 5)] = 0,
+            [(1, 6)] = 0,
+            [(1, 7)] = 0,
+            [(1, 8)] = 0,
+            [(2, 1)] = 5,
+            [(2, 2)] = 10,
+            [(2, 3)] = 10,
+            [(2, 4)] = -20,
+            [(2, 5)] = -20,
+            [(2, 6)] = 10,
+            [(2, 7)] = 10,
+            [(2, 8)] = 5,
+            [(3, 1)] = 5,
+            [(3, 2)] = -5,
+            [(3, 3)] = -10,
+            [(3, 4)] = 0,
+            [(3, 5)] = 0,
+            [(3, 6)] = -10,
+            [(3, 7)] = -5,
+            [(3, 8)] = 5,
+            [(4, 1)] = 0,
+            [(4, 2)] = 0,
+            [(4, 3)] = 0,
+            [(4, 4)] = 20,
+            [(4, 5)] = 20,
+            [(4, 6)] = 0,
+            [(4, 7)] = 0,
+            [(4, 8)] = 0,
+            [(5, 1)] = 5,
+            [(5, 2)] = 5,
+            [(5, 3)] = 10,
+            [(5, 4)] = 25,
+            [(5, 5)] = 25,
+            [(5, 6)] = 10,
+            [(5, 7)] = 5,
+            [(5, 8)] = 5,
+            [(6, 1)] = 10,
+            [(6, 2)] = 10,
+            [(6, 3)] = 20,
+            [(6, 4)] = 30,
+            [(6, 5)] = 30,
+            [(6, 6)] = 20,
+            [(6, 7)] = 10,
+            [(6, 8)] = 10,
+            [(7, 1)] = 50,
+            [(7, 2)] = 50,
+            [(7, 3)] = 50,
+            [(7, 4)] = 50,
+            [(7, 5)] = 50,
+            [(7, 6)] = 50,
+            [(7, 7)] = 50,
+            [(7, 8)] = 50,
+            [(8, 1)] = 0,
+            [(8, 2)] = 0,
+            [(8, 3)] = 0,
+            [(8, 4)] = 0,
+            [(8, 5)] = 0,
+            [(8, 6)] = 0,
+            [(8, 7)] = 0,
+            [(8, 8)] = 0,
+        };
 
         public Pawn((int, int) position, (int, int) aiposition, List<Move> moves, bool colour, Char charRep = 'P', int pointsValue=100, bool enPassant=false, bool king = false, bool firstMove=true, bool isPinned = false)
             :base(position, aiposition, moves, colour, charRep, pointsValue, enPassant, king, firstMove, isPinned)
@@ -111,6 +177,15 @@ namespace ChessV2
                 EnPassant = true;
                 Moves.Add(new Move(this, (posToCheck.Item1, posToCheck.Item2 + (Colour ? 1 : -1)), AIposition.Item1, AIposition.Item2, true));
             }
+        }
+
+        public override double Evaluate()
+        {
+            if (!Colour)
+            {
+                return PointsValue + Squares[(AIposition.Item1, Ranks[AIposition.Item2 - 1])];
+            }
+            return PointsValue + Squares[AIposition];
         }
     }   
 }
